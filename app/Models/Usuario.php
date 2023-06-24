7d88a78edde0e84ad4a7c394894396a5414c4631
Carlos <carlos.9942@gmail.com>
2023-06-24 02:35:30 -0400

    refactor: Rename model to follow Laravel conventions and establish table relationship

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Usuarios extends Model
+class Usuario extends Model
 {
     use HasFactory;
     protected $table = 'usuario';
     protected $primaryKey = 'id';
     public $timestamps = false;
+
+    public function profile()
+    {
+        return $this->belongsTo(Perfil::class, 'idPerfil');
+    }
 }
