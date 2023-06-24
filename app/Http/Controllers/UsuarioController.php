055b6692b9c666069ae938176da3db8601e416c5
Carlos <carlos.9942@gmail.com>
2023-06-23 23:09:58 -0400

    fix: Add calculation and formatting of RUN'

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Usuarios;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Utiles;
 
 class UsuarioController extends Controller
 {
@@ -15,8 +16,42 @@ class UsuarioController extends Controller
     public function index()
     {
         $usuarios = Usuarios::get();
+        foreach ($usuarios as $usuario){
+            $run = $usuario->rut;
+            $dv = self::calcularDV($run);
+            $formattedRun = '';
+            while (strlen($run) > 3) {
+                $formattedRun = '.' . substr($run, -3) . $formattedRun;
+                $run = substr($run, 0, -3);
+            }
+            $usuario->rut = $run . $formattedRun . '-' . $dv;
+        }
         return view('usuarios.index')
             ->with('usuarios', $usuarios);
+    }
+
+    function calcularDV($run) {
+        $run = str_replace('.', '', $run);
+        $run = str_replace('-', '', $run);
+
+        $reversedRun = strrev($run);
+        $factor = 2;
+        $sum = 0;
+
+        for ($i = 0; $i < strlen($reversedRun); $i++) {
+            $sum += intval($reversedRun[$i]) * $factor;
+            $factor = $factor % 7 === 0 ? 2 : $factor + 1;
+        }
+
+        $dv = 11 - ($sum % 11);
+
+        if ($dv === 10) {
+            $dv = 'K';
+        } elseif ($dv === 11) {
+            $dv = '0';
+        }
+
+        return $dv;
     }
 
     /**
