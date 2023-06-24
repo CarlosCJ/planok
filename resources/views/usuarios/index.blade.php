3989cf854f038fc4dcb164136a1e86a35191e2cb
Carlos <carlos.9942@gmail.com>
2023-06-23 21:38:02 -0400

    add route user with model, controller and view

---
+++ resources/views/usuarios/index.blade.php
@@ -0,0 +1,38 @@
+<?php
+ini_set('max_execution_time', 800);
+?>
+@extends('layouts.layout')
+@section('title', 'Usuarios')
+@section('content')
+
+    <div class="container pt-md-5 pb-md-5 pt-3 pb-3">
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <th scope="col">RUN</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Perfil</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Opciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($usuarios as $usuario)
+                <tr>
+                    <td>{{ $usuario->rut }}</td>
+                    <td>{{ mb_convert_case($usuario->nombre, MB_CASE_TITLE, "UTF-8") }} {{ mb_convert_case($usuario->apellido, MB_CASE_TITLE, "UTF-8") }}</td>
+                    <td>{{ $usuario->correo }}</td>
+                    <td>{{ $usuario->idPerfil }}</td>
+                    <td>{{ $usuario->estado }}</td>
+                    <td>
+
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+@stop
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UsuarioController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios');
