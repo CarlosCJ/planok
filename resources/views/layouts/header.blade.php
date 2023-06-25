31e36d0cceb70bd66012be761852d444dfb8d1a3
Carlos <carlos.9942@gmail.com>
2023-06-25 15:09:11 -0400

    fix: Correct menu and homepage

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,36 +8,24 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Usuarios</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('usuarios') }}">Usuarios</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Cotización</a>
+                        <a class="nav-link" href="{{ route('cotizaciones') }}">Cotización</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Cliente</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Producto</a>
+                        <a class="nav-link" href="{{ route('clientes') }}">Cliente</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
+                            Producto
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><a class="dropdown-item" href="{{ route('productos') }}">Listar Producto</a></li>
+                            <li><a class="dropdown-item" href="{{ route('cantProductos') }}">Cantidad Producto</a></li>
                         </ul>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
             </div>
         </div>
     </nav>
